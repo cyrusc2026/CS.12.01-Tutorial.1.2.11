2eaad9defdad696b6cb947066434087935614417
cyrusc2026 <cyrusc2026@student.cis.edu.hk>
2024-09-01 19:01:15 +0800

    Commit 1!!!

---
+++ src/DiverseArray.java
@@ -0,0 +1,27 @@
+public class DiverseArray {
+    public static int arraySum (int[] array){
+        int sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
+    }
+    public static int[] rowSums (int[][] array){
+        int[] answer = new int[array.length];
+        for (int row = 0; row < array.length; row++){
+            answer[row] = DiverseArray.arraySum(array[row]);
+        }
+        return answer;
+    }
+    public static boolean isDiverse (int[][] array){
+        int[] sums = DiverseArray.rowSums(array);
+        for (int i = 0; i < sums.length; i++){
+            for (int j = i+1; j < sums.length; j++){
+                if (sums[i] == sums[j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
